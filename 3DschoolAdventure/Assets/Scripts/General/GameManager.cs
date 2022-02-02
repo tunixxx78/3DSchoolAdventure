00cf3c6c1709580d9479ba831dd277d090f48321
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2022-02-02 15:11:50 +0200

    First prototype

---
+++ 3DschoolAdventure/Assets/Scripts/General/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameOverPanel, winningPanel;
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void TryAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ 3DschoolAdventure/Assets/Scripts/Obstaclles/RotatingObstacle.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingObstacle : MonoBehaviour
+{
+    [SerializeField] float rotatingSpeed, rotatingBreak;
+    bool canRotate;
+
+    private void Start()
+    {
+        canRotate = true;
+    }
+
+    private void Update()
+    {
+        if (canRotate)
+        {
+            transform.Rotate(Vector3.down * rotatingSpeed);
+        }
+        if (Input.GetKey(KeyCode.P))
+        {
+            canRotate = false;
+        }
+        if (Input.GetKey(KeyCode.O))
+        {
+            canRotate = true;
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            canRotate = false;
+
+            StartCoroutine(StartRotating());
+        }
+    }
+
+    IEnumerator StartRotating()
+    {
+        yield return new WaitForSeconds(rotatingBreak);
+
+        canRotate = true;
+    }
+}
