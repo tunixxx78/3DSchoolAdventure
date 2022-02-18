c0f8ae1c658faa0537a9c6ef02a960736a254ff3
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2022-02-18 17:51:28 +0200

    Updates 18.2.2022
    New object added
    terrain re-started
    First level design tryout started

---
+++ 3DschoolAdventure/Assets/Scripts/Collectibles/DashObeject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashObeject : MonoBehaviour
+{
+    public int dashIncreaseAmount;
+    public bool hasDashBoostAddOn = false;
+}
