17bec0fdef3df047788037a48ca7b061fa8d23dd
tuiskuhjoki <tuiskuh@gmail.com>
2022-02-19 22:09:44 +0200

    DashUI updated

--- 3DschoolAdventure/Assets/Scripts/UI/DashUI.cs
+++ 3DschoolAdventure/Assets/Scripts/UI/DashUI.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class DashUI : MonoBehaviour
 {
-    public int dashNum;
     public TuroPlayerMovement playerScript;
     public int activePlayer;
     public GameObject[] players;
+
+    public int dashNum;
+    public GameObject[] dashImage;
 
     private void Awake()
     {
@@ -19,9 +21,36 @@ public class DashUI : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         dashNum = playerScript.dashAmount;
+        DashImageActivation();
+    }
+
+    private void DashImageActivation()
+    {
+        switch (dashNum)
+        {
+            case 0:
+                dashImage[0].SetActive(false);
+                dashImage[1].SetActive(false);
+                dashImage[2].SetActive(false);
+                break;
+            case 1:
+                dashImage[0].SetActive(true);
+                dashImage[1].SetActive(false);
+                dashImage[2].SetActive(false);
+                break;
+            case 2:
+                dashImage[0].SetActive(true);
+                dashImage[1].SetActive(true);
+                dashImage[2].SetActive(false);
+                break;
+            case 3:
+                dashImage[0].SetActive(true);
+                dashImage[1].SetActive(true);
+                dashImage[2].SetActive(true);
+                break;
+        }
     }
 }
