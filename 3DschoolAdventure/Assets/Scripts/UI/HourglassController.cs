4c3d101f9eb424db52938c38dc069ea44b63cab4
tuiskuhjoki <tuiskuh@gmail.com>
2022-03-12 18:21:07 +0200

    UI Prefabs sizes changed. Time script changes

--- 3DschoolAdventure/Assets/Scripts/UI/HourglassController.cs
+++ 3DschoolAdventure/Assets/Scripts/UI/HourglassController.cs
@@ -23,28 +23,32 @@ public class HourglassController : MonoBehaviour
             playerScript = players[activePlayer].GetComponent<TuroPlayerMovement>();
             hourGlass.value = playerScript.startTime;
             currentTime = hourGlass.value;
+            //timeText.text = playerScript.startTime.ToString(string.Format("{0:0}", playerScript.startTime));
         }
     }
 
     private void Update()
     {
         currentTime = playerScript.currentTime;
+
+        float min = Mathf.FloorToInt(currentTime / 60);
+        float sec = Mathf.FloorToInt(currentTime % 60);
+        timeText.text = string.Format("{0:00}:{1:00}", min, sec);
+
         hourGlass.value = currentTime;
-        ActivateTimeText();
     }
 
-    private void ActivateTimeText()
-    {
-        if (currentTime <= 9 && currentTime > 0)
-        {
-            timeText.gameObject.SetActive(true);
-            timeText.text = currentTime.ToString(string.Format("{0:0}", currentTime));
-        }
-        else
-        {
-            timeText.gameObject.SetActive(false);
-            timeText.text = 9.ToString();
-        }
-
-    }
+    //private void TimeText()
+    //{
+    //    if (currentTime <= 9 && currentTime > 0)
+    //    {
+    //        timeText.gameObject.SetActive(true);
+    //        timeText.text = currentTime.ToString(string.Format("{0:0}", currentTime));
+    //    }
+    //    else
+    //    {
+    //        timeText.gameObject.SetActive(false);
+    //        timeText.text = 9.ToString();
+    //    }
+    //}
 }
