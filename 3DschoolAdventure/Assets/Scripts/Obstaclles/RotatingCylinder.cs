b6ec01df90d988c2aa822adf753b237b431c5b72
tuiskuhjoki <tuiskuh@gmail.com>
2022-03-27 21:45:23 +0300

    RotatingCylinder attempt

---
+++ 3DschoolAdventure/Assets/Scripts/Obstaclles/RotatingCylinder.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingCylinder : MonoBehaviour
+{
+    public bool rotating;
+    public float rotatingSpeed = 1f;
+    public CharacterController player;
+    public Vector3 playerPos;
+    public bool rotateRight, rotateLeft;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GetComponent<TuroPlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotating && rotateLeft)
+        {
+            transform.Rotate(0f, -1 * player.GetComponent<TuroPlayerMovement>().gravity * player.GetComponent<TuroPlayerMovement>().jumpForce/* * rotatingSpeed*/ * Time.deltaTime, 0f, Space.Self);
+        }
+        if (rotating && rotateRight)
+        {
+            transform.Rotate(0f, 1 * player.GetComponent<TuroPlayerMovement>().gravity * player.GetComponent<TuroPlayerMovement>().jumpForce/* * rotatingSpeed*/ * Time.deltaTime, 0f, Space.Self);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            playerPos = other.gameObject.transform.position;
+            player = other.GetComponent<TuroPlayerMovement>().myCC;
+            rotating = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        rotating = false;
+    }
+}
