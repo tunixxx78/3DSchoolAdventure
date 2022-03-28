f5da1a291cefa5d58ce68d2ccad049413e464b1a
tuiskuhjoki <tuiskuh@gmail.com>
2022-03-28 10:36:14 +0300

    RotatingCylinder update

--- 3DschoolAdventure/Assets/Scripts/Obstaclles/RotatingCylinder.cs
+++ 3DschoolAdventure/Assets/Scripts/Obstaclles/RotatingCylinder.cs
@@ -29,7 +29,7 @@ public class RotatingCylinder : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
