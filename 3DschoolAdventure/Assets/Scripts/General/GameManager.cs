bd2b5cc60b36954a2ec367be6614b13a2453c0c9
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2022-04-24 13:14:12 +0300

    Fix for points system

--- 3DschoolAdventure/Assets/Scripts/General/GameManager.cs
+++ 3DschoolAdventure/Assets/Scripts/General/GameManager.cs
@@ -26,6 +26,7 @@ public class GameManager : MonoBehaviour
         if(SaveSystem.savingInstance.continueGame == false)
         {
             newGame = true;
+            PlayerPrefs.SetInt("PointsToNextLevel", 0);
         }
         
     }
