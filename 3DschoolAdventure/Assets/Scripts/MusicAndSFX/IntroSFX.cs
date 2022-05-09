e82a71a2585a5c5a73f6e6dc8cda750a9ecba4d9
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2022-05-09 09:35:38 +0300

    Custom cursor added

--- 3DschoolAdventure/Assets/Scripts/MusicAndSFX/IntroSFX.cs
+++ 3DschoolAdventure/Assets/Scripts/MusicAndSFX/IntroSFX.cs
@@ -8,8 +8,28 @@ public class IntroSFX : MonoBehaviour
     [SerializeField] AudioSource voice1, voice2, voice3;
     [SerializeField] AudioSource glass, rotating;
 
-    public void Talking()
+    public void TalkingOne()
     {
-        
+        voice1.Play();
+    }
+
+    public void TalkingTwo()
+    {
+        voice2.Play();
+    }
+
+    public void TalkingThree()
+    {
+        voice3.Play();
+    }
+
+    public void Glass()
+    {
+        glass.Play();
+    }
+
+    public void Rotating()
+    {
+        rotating.Play();
     }
 }
